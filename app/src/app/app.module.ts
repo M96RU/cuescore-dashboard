7e8d5169173469668ecb40e067ee31ffbea85d1e
Manuel RUSSO <manuel.russo@worldline.com>
2023-02-17 11:18:45 +0100

    Add material angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {IntegrationService} from 'src/app/integration/integration-service';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -12,8 +13,9 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     IntegrationService
