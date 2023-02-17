0f5e280365ffd2da1acf63351507c821287f4059
Manuel RUSSO <manuel.russo@worldline.com>
2023-02-17 14:18:59 +0100

    Add refresh button

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -37,6 +38,7 @@ import {TournamentComponent} from './tournament/tournament.component';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule,
   ],
   providers: [
     IntegrationService,
