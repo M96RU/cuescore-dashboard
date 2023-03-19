cde4dcdfaf0194e8ee869ce69d33659d01dfdb7d
Manuel RUSSO <manuel.russo@worldline.com>
2023-03-19 14:12:19 +0100

    Add incoming matches of player

--- app/src/app/player-detail/player-detail.component.ts
+++ app/src/app/player-detail/player-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {Match} from 'src/app/model/match';
 import {Player} from 'src/app/model/player';
 
 @Component({
@@ -11,8 +12,11 @@ export class PlayerDetailComponent implements OnInit {
 
   playing = false;
 
+  incomingMatches: Match[] = [];
+
   constructor(@Inject(MAT_DIALOG_DATA) public player: Player) {
     this.playing = player.inProgress.length > 0;
+    this.incomingMatches = player.matches.filter(match => match.status != 'finished' && match.status != 'playing')
     console.log('player', player);
   }
 
